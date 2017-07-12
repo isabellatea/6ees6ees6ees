b69cb471b1bd8c0b08663242edb73112c82261bd
Isabella Tea <tea.isabella@gmail.com>
2017-07-11 20:40:20 -0700

    working implementation of class inheritance using ES6

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,14 @@
-class HoneyMakerBee {
-  // TODO..
+class HoneyMakerBee extends Bee{
+  constructor(age, job, color, food, eat, honeyPot) {
+    super(Bee);
+    this.age = 10;
+    this.job = "make honey";
+    this.honeyPot = 0;
+  }
+  makeHoney() {
+    this.honeyPot++;
+  }
+  giveHoney() {
+    this.honeyPot--;
+  }
 };
